70547f53b62ced51a86b682acc0bcced7c0cbd56
mansi0 <mansimandhare.mm@gmail.com>
2020-02-04 22:09:30 +0530

    hotel_food values

--- demo/src/main/resources/data.sql
+++ demo/src/main/resources/data.sql
@@ -98,8 +98,167 @@ insert into hotel values(
 
 
 
+/*food*/
+insert into food values('1001','Pohe','Breakfast','Light');
+insert into food values('1002','Upma','Breakfast','Light');
+insert into food values('1003','Misal','Breakfast','Heavy');
+insert into food values('1004','Idli','Breakfast','Heavy');
+insert into food values('1005','Dosa','Breakfast','Light');
 
+insert into food values('1006','Manchow Soup','Starters','Soup');
+insert into food values('1007','Manchurian','Starters','Chinese');
+insert into food values('1008','Paneer 65','Starters','Punjabi');
+insert into food values('1009','Paneer Chilli Dry','Starters','Punjabi');
+insert into food values('1010','Corn Soup','Starters','Soup');
+insert into food values('1011','Veg Lollipop','Starters','Chinese');
+insert into food values('1012','Spring Roll','Starters','Chinese');
 
+insert into food values('1013','Roti','Main Course','Roti');
+insert into food values('1014','Simple Rice','Main Course','Rice');
+insert into food values('1015','Butter Roti','Main Course','Roti');
+insert into food values('1016','Jeera rice','Main Course','Rice');
+insert into food values('1017','Paneer Tikka','Main Course','Gravy');
+insert into food values('1018','Kaju Masala','Main Course','Gravy');
+insert into food values('1019','Veg Biryani','Main Course','Rice');
+insert into food values('1020','Chicken Biryani','Main Course','Rice');
+insert into food values('1021','Mutton Biryani','Main Course','Rice');
+insert into food values('1022','Paneer Masala','Main Course','Gravy');
+insert into food values('1023','Veg Kolhapuri','Main Course','Gravy');
+insert into food values('1024','Veg Maratha','Main Course','Gravy');
+insert into food values('1025','Kaju Curry','Main Course','Gravy');
 
+insert into food values('1026','Gulam Jamun','Desserts','Sweets');
+insert into food values('1027','Chocolate Pastry','Desserts','Cake');
+insert into food values('1028','Carrot Halwa','Desserts','Sweets');
+insert into food values('1029','Chocolate Balls','Desserts','Cake');
+insert into food values('1030','Rasgulla','Desserts','Sweets');
 
-    
+insert into food values('1031','Panner Tandoori Pizza','Fast Food','Pizza');
+insert into food values('1032','Paneer Makhni Pizza','Fast Food','Pizza');
+insert into food values('1033','Aloo Tikki Burger','Fast Food','Burger');
+insert into food values('1034','Veggie Burger','Fast Food','Burger');
+insert into food values('1035','Salty Fries','Fast Food','Fries');
+insert into food values('1036','Cheese Fries','Fast Food','Fries');
+insert into food values('1037','Super Nachos','Fast Food','Nachos');
+insert into food values('1038','Loaded Veggie Nachos','Fast Food','Nachos');
+insert into food values('1039','White Pasta ','Fast Food','Pasta');
+insert into food values('1040','Masala Pasta','Fast Food','Pasta');
+insert into food values('1041','Grill Sandwitch','Fast Food','Sandwitch');
+insert into food values('1042','Veg Sandwitch','Fast Food','Sandwitch');
+insert into food values('1043','Veg Momos','Fast Food','Momos');
+insert into food values('1044','Chicken Momos','Fast Food','Momos');
+
+insert into food values('1045','Vin de pays','Drinks With Alcohol','Wine');
+insert into food values('1046','Dubble','Drinks With Alcohol','Beer');
+insert into food values('1047','Martini','Drinks With Alcohol','Vodka');
+insert into food values('1048','Burgundy wine','Drinks With Alcohol','Wine');
+insert into food values('1049','Brown ale','Drinks With Alcohol','Beer');
+
+insert into food values('1050','Hot Cappuccino','Cappuccino','Hot');
+insert into food values('1051','Iced Cappuccino','Cappuccino','Cold');
+
+insert into food values('1052','Mango Juice','Juice','Fruit Juice');
+insert into food values('1053','Lemon Soda','Juice','Aerated Juice');
+insert into food values('1054','Water Melon','Juice','Fruit Juice');
+insert into food values('1055','Chickoo Juice','Juice','Fruit Juice');
+insert into food values('1056','Fresh Fruit Juice','Juice','Aerated Juice');
+
+insert into food values('1057','Blue Mojito','Mocktail','Mojito');
+insert into food values('1058','Monin Mojito Mint Syrup','Mocktail','Mojito');
+insert into food values('1059','Princess Nojito','Mocktail','Nojito');
+insert into food values('1060','Sainsbury Nojito','Mocktail','Nojito');
+
+insert into food values('1061','Chocolate Cornetto','Ice Cream','Cone');
+insert into food values('1062','Butter Scotch','Ice Cream','Cone');
+insert into food values('1063','Vanilla Cups','Ice Cream','Cup');
+insert into food values('1064','Pista Cups','Ice Cream','Cup');
+insert into food values('1065','Chocolate Chips','Ice Cream','Family Pack');
+insert into food values('1066','Cream and Cookies','Ice Cream','Family Pack');
+insert into food values('1067','Magnum','Ice Cream','Candy');
+insert into food values('1068','Chocobar','Ice Cream','Candy');
+insert into food values('1069','Malai Kulfi','Ice Cream','Kulfi');
+insert into food values('1070','Mix Kulfi','Ice Cream','Kulfi');
+
+/*hotel_food*/
+insert into hotel_food values('2000','101','1001','Garnished with cheese and coriander','$40','2');
+insert into hotel_food values('2001','101','1050','Extra Chcocolate','$50','1');
+insert into hotel_food values('2002','102','1002','Garnished with cheese and coriander','$50','2');
+insert into hotel_food values('2003','102','1004','Garnished with cheese and coriander','$60','3');
+insert into hotel_food values('2004','103','1003','Garnished with cheese and coriander','$60','2');
+insert into hotel_food values('2005','103','1005','Garnished with cheese and coriander','$50','1');
+insert into hotel_food values('2006','104','1006','Garnished with cheese and coriander','$50','2');
+insert into hotel_food values('2007','104','1007','Garnished with cheese and coriander','$100','3');
+insert into hotel_food values('2008','105','1008','Garnished with cheese and coriander','$80','1');
+insert into hotel_food values('2009','105','1009','Garnished with cheese and coriander','$100','2');
+insert into hotel_food values('2010','106','1010','Garnished with cheese and coriander','$90','3');
+insert into hotel_food values('2011','106','1011','Garnished with cheese and coriander','$110','1');
+insert into hotel_food values('2012','107','1012','Garnished with cheese and coriander','$80','2');
+insert into hotel_food values('2013','107','1013','Garnished with cheese and coriander','$90','3');
+insert into hotel_food values('2014','108','1014','Garnished with cheese and coriander','$50','1');
+insert into hotel_food values('2015','108','1015','Garnished with cheese and coriander','$99','2');
+insert into hotel_food values('2016','109','1016','Garnished with cheese and coriander','$100','3');
+insert into hotel_food values('2017','109','1017','Garnished with cheese and coriander','$120','2');
+insert into hotel_food values('2018','110','1018','Garnished with cheese and coriander','$130','1');
+insert into hotel_food values('2019','111','1019','Garnished with cheese and coriander','$150','3');
+insert into hotel_food values('2020','112','1020','Garnished with cheese and coriander','$145','2');
+insert into hotel_food values('2021','113','1021','Garnished with cheese and coriander','$145','1');
+insert into hotel_food values('2022','114','1022','Garnished with cheese and coriander','$140','2');
+insert into hotel_food values('2023','115','1023','Garnished with cheese and coriander','$140','1');
+insert into hotel_food values('2024','116','1024','Garnished with cheese and coriander','$140','3');
+insert into hotel_food values('2025','110','1025','Garnished with cheese and coriander','$90','2');
+insert into hotel_food values('2026','111','1026','Garnished with cheese and coriander','$70','2');
+insert into hotel_food values('2027','112','1027','Garnished with cheese and coriander','$80','1');
+insert into hotel_food values('2028','113','1028','Garnished with cheese and coriander','$50','1');
+insert into hotel_food values('2029','114','1029','Garnished with cheese and coriander','$45','2');
+insert into hotel_food values('2030','115','1030','Garnished with cheese and coriander','$45','3');
+insert into hotel_food values('2031','116','1031','Garnished with cheese and coriander','$50','1');
+insert into hotel_food values('2032','101','1032','Garnished with cheese and coriander','$49','2');
+insert into hotel_food values('2033','102','1033','Garnished with cheese and coriander','$60','3');
+insert into hotel_food values('2034','103','1034','Garnished with cheese and coriander','$70','2');
+insert into hotel_food values('2035','104','1035','Garnished with cheese and coriander','$45','1');
+insert into hotel_food values('2036','105','1036','Garnished with cheese and coriander','$45','1');
+insert into hotel_food values('2037','106','1037','Garnished with cheese and coriander','$50','1');
+insert into hotel_food values('2038','107','1038','Garnished with cheese and coriander','$60','3');
+insert into hotel_food values('2039','108','1039','Garnished with cheese and coriander','$45','2');
+insert into hotel_food values('2040','109','1040','Garnished with cheese and coriander','$65','3');
+insert into hotel_food values('2041','110','1041','Garnished with cheese and coriander','$55','1');
+insert into hotel_food values('2042','111','1042','Garnished with cheese and coriander','$50','1');
+insert into hotel_food values('2043','112','1043','Garnished with cheese and coriander','$70','2');
+insert into hotel_food values('2044','113','1044','Garnished with cheese and coriander','$80','2');
+insert into hotel_food values('2045','114','1045','Extra 10% Drink','$200','1');
+insert into hotel_food values('2046','115','1046','Extra 10% Drink','$150','1');
+insert into hotel_food values('2047','116','1047','Extra 10% Drink','$200','2');
+insert into hotel_food values('2048','101','1048','Extra 10% Drink','$150','1');
+insert into hotel_food values('2049','102','1049','Extra 10% Drink','$200','1');
+insert into hotel_food values('2050','103','1050','Extra 10%','$45','1');
+insert into hotel_food values('2051','104','1051','Extra 10%','$45','2');
+insert into hotel_food values('2052','105','1052','Extra 10%','$30','3');
+insert into hotel_food values('2053','106','1053','Extra 10%','$30','2');
+insert into hotel_food values('2054','107','1054','Extra 10%','$40','1');
+insert into hotel_food values('2055','108','1055','Extra 10%','$90','2');
+insert into hotel_food values('2056','109','1056','Extra 10%','$90','3');
+insert into hotel_food values('2057','110','1057','Extra 10%','$80','2');
+insert into hotel_food values('2058','111','1058','Extra 10%','$80','1');
+insert into hotel_food values('2059','112','1059','Extra 10%','$75','2');
+insert into hotel_food values('2060','113','1060','Extra 10%','$70','2');
+insert into hotel_food values('2061','114','1061','Extra 10%','$80','1');
+insert into hotel_food values('2062','115','1062','Extra 10%','$70','2');
+insert into hotel_food values('2063','116','1063','Extra 10%','$90','2');
+insert into hotel_food values('2064','101','1064','Extra 10%','$50','3');
+insert into hotel_food values('2065','102','1065','Extra 10%','$100','3');
+insert into hotel_food values('2066','103','1066','Extra 10%','$100','2');
+insert into hotel_food values('2067','104','1067','Extra 10%','$70','1');
+insert into hotel_food values('2068','105','1068','Extra 10%','$70','1');
+insert into hotel_food values('2069','106','1069','Extra 10%','$80','2');
+insert into hotel_food values('2070','107','1070','Extra 10%','$80','2');
+insert into hotel_food values('2071','108','1001','Extra 10%','$90','2');
+insert into hotel_food values('2072','109','1020','Extra 10%','$100','2');
+insert into hotel_food values('2073','110','1014','Extra 10%','$90','2');
+insert into hotel_food values('2074','111','1015','Extra 10%','$70','2');
+insert into hotel_food values('2075','112','1010','Extra 10%','$90','2');
+insert into hotel_food values('2076','113','1050','Extra 10%','$100','2');
+insert into hotel_food values('2077','114','1030','Extra 10%','$80','3');
+insert into hotel_food values('2078','115','1034','Extra 10%','$90','1');
+insert into hotel_food values('2079','116','1025','Extra 10%','$70','2');
+insert into hotel_food values('2080','101','1026','Extra 10%','$70','1');
+
